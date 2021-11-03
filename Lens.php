20cdc53bf8ae0e14856711c1a37201fdbaa210e8
dizhang <zdi@live.com>
2021-11-03 17:58:54 +0800

    Update Lens.php

--- Lens.php
+++ Lens.php
@@ -15,7 +15,7 @@ https://github.com/zzd/photo-page-for-typecho
 <?php
 function usePublicCdn()
 {
-	echo "https://cdn.jsdelivr.net/gh/zzd/photo-page-for-typecho@2.0";
+	echo "https://cdn.jsdelivr.net/gh/zzd/photo-page-for-typecho@master";
 }
 ?>
 <!-- 公共CDN结束 -->
